f340a71749b6265900857559978589d3d0e7bab6
Matinou Badarou <badaroumatinou@gmail.com>
2023-07-05 15:09:53 +0100

    ajout du fichier filtre.php

---
+++ filtre.php
@@ -0,0 +1,18 @@
+<div class="filtre">
+    <h4>Filtrez les services de plomberies par ville ou par prix</h4>
+    <form action="">
+        <div class="form-part">
+            <h5>Selectionnez la ville ou vous etes</h5>
+            <select name="" id="">
+                <option value="">Porto novo</option>
+                <option value="">Cotonou</option>
+            </select>
+        </div>
+        <div class="form-part">
+            <h5>Entrez votre budget minimale et votre budget maximale</h5>
+            <input type="text" placeholder="Montant minimale">
+            <input type="text" placeholder="Montant maximale">
+        </div>
+        <button>Cherchez selon mes critères</button>
+    </form>
+</div>
