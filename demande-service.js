17917c69e86cee2bf2811198d06c6d12e4d1e451
Matinou Badarou <badaroumatinou@gmail.com>
2023-08-04 23:54:56 +0100

    add demande-service.js

---
+++ demande-service.js
@@ -0,0 +1,8 @@
+var services = document.querySelectorAll('.service')
+/*services.forEach(service=>{
+    service.addEventListener('click',function(e){
+        e.preventDefault()
+        e.target.parentElement.style.border = "3px solid white"
+        document.querySelector('.services').style.width = '74%'
+    })
+})*/
