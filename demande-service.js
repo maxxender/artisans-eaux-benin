ce2dbfc0ae73076e416ac0445006ec586e187d04
Matinou Badarou <badaroumatinou@gmail.com>
2023-08-06 01:54:36 +0100

    form demande serice ajuster top

--- demande-service.js
+++ demande-service.js
@@ -1,8 +1,8 @@
 var services = document.querySelectorAll('.service')
-/*services.forEach(service=>{
+services.forEach(service=>{
     service.addEventListener('click',function(e){
         e.preventDefault()
         e.target.parentElement.style.border = "3px solid white"
-        document.querySelector('.services').style.width = '74%'
+        window.location.href = "./#form-demande-service"
     })
-})*/
+})
---
+++ demande-service.php
@@ -0,0 +1,32 @@
+<form action="" id="form-demande-service">
+    <div class="">
+        <h4>Vous avez choisi le service</h4>
+        <p>Réalisation d'un système d'arrosage</p>
+    </div>
+          <div class="form-div-group">
+          <h6>Dans quel département ?</h6>
+          <select name="" id="">
+            <option value="">Choix du département</option>
+            <option value="">Oéme</option>
+            <option value="">Palteau</option>
+          </select>
+          </div>
+          <div>
+            <h6>Dans quel ville ?</h6>
+            <select name="" id="">
+              <option value="">Choix de la ville</option>
+              <option value="">Porto Novo</option>
+              <option value="">Cotonou</option>
+            </select>
+          </div>
+          <div>
+            <h6>Le contact pour vous joindre</h6>
+            <input type="text">
+          </div>
+          <div>
+            <h6>Souhaitez vous etres contactez par appels ou messages whatsapp</h6>
+            <a href="" class="btn-type-contact">Contact par appel telephonique</a>
+            <a href="" class="btn-type-contact">Contact par message Whatsapp</a>
+          </div>
+          <button type="submit" class="btn-submit">Lancez un appel d'offre</button>
+        </form>
