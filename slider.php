fa9bd8cbf2eb608903697cf5215cde51d959d32b
Matinou Badarou <badaroumatinou@gmail.com>
2023-08-21 15:57:38 +0100

    slider

---
+++ slider.php
@@ -0,0 +1,8 @@
+<div class="slider">
+            <div class="slides">
+              <div class="slide"><img src="images/services.jpg" alt=""></div>
+              <div class="slide"><img src="images/piscine.jpeg" alt=""></div>
+              <div class="slide"><img src="images/citerne enterré.jpeg" alt=""></div>
+              <div class="slide"><img src="images/services.jpg" alt=""></div>
+            </div>
+        </div>
