d40d43f3aabd0495fa5b3755a664646e1e6b9a04
Matinou Badarou <badaroumatinou@gmail.com>
2023-08-21 16:04:09 +0100

    departements-benin

---
+++ departements-benin.php
@@ -0,0 +1,15 @@
+<?php
+$departements = [
+    "Alibori",
+    "Atacora",
+    "Atlantique",
+    "Borgou",
+    "Collines",
+    "Couffo",
+    "Donga",
+    "Littoral",
+    "Mono",
+    "Ouémé",
+    "Plateau",
+    "Zou"
+];
